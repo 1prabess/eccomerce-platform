8a818fcbe18bbf239e71cf216d5f9f18b6bf9c6c
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-05-26 04:42:56 +0545

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,17 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDb from "./config/db.js";
+import responseFormatter from "./middlewares/responseFormatter.js";
 
 dotenv.config();
 const PORT = process.env.PORT;
 
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
+app.use(responseFormatter);
 
 app.listen(PORT, async () => {
   try {
-    connectDb();
+    await connectDb();
     console.log(`App is listening on port: ${PORT}`);
   } catch (error) {
     console.log("Error in listening by app");
