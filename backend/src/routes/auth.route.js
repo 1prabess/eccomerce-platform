c2e957548f1adb0365a746fb7f77d0bab9653749
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-05-27 10:26:30 +0545

    Update auth.route.js

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,15 +1,14 @@
 import express from "express";
-import {
-  adminSignup,
-  forgotPassword,
-  logout,
-  resetPassword,
-  signin,
-  signup,
-  verifyAccount,
-} from "../controllers/auth.controller.js";
+
 import authorizeRoles from "../middlewares/authorize.middleware.js";
 import authenticate from "../middlewares/authenticate.middleware.js";
+import { signup } from "../controllers/auth/signup.controller.js";
+import { adminSignup } from "../controllers/auth/adminSignup.controller.js";
+import { signin } from "../controllers/auth/signin.controller.js";
+import { logout } from "../controllers/auth/logout.controller.js";
+import { verifyAccount } from "../controllers/auth/verifyAccount.controller.js";
+import { forgotPassword } from "../controllers/auth/forgotPassword.controller.js";
+import { resetPassword } from "../controllers/auth/resetPassword.controller.js";
 
 const authRouter = express.Router();
 
