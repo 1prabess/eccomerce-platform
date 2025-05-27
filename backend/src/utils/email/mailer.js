83c2a76cc2a8d7cd9acf123132764370c584e52a
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-05-27 12:23:07 +0545

    Create mailer.js

---
+++ backend/src/utils/email/mailer.js
@@ -0,0 +1,31 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function mailer(template, recipient, subject, text) {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.PLATFORM_EMAIL,
+        pass: process.env.PLATFORM_EMAIL_APP_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: `"Ecommerce App" <${process.env.PLATFORM_EMAIL}>`,
+      to: recipient,
+      replyTo: process.env.PLATFORM_EMAIL,
+      subject,
+      text: text,
+      html: template,
+    };
+
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.error("❌ Failed to send email:", error);
+  }
+}
+
+export default mailer;
