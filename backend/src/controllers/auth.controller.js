e174a45b063b1948f49d148d8267a2be41786d79
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-05-27 10:00:42 +0545

    Add verify account feature

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import generateAndSetToken from "../utils/generateAndSetToken.js";
+import crypto from "crypto";
 
 // ___________Sign up_________________
 export const signup = async (req, res) => {
@@ -139,7 +140,7 @@ export const signin = async (req, res) => {
     if (!isPasswordValid)
       return res
         .status(StatusCodes.CONFLICT)
-        .json({ message: "Invalid credentials ." });
+        .json({ message: "Invalid credentials." });
 
     generateAndSetToken(res, user._id);
 
@@ -169,6 +170,45 @@ export const logout = async (req, res) => {
     });
   } catch (error) {
     console.log("Error in logout: ", error);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Something went wrong. Please try again later." });
+  }
+};
+
+// ___________Verify Account_________________
+export const verifyAccount = async (req, res) => {
+  try {
+    // Get token from params
+    const { token } = req.params;
+
+    if (!token)
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "No verification token provided." });
+
+    // Get the user only if the token is valid
+    const user = await User.findOne({
+      verificationToken: token,
+      verificationTokenExpiresAt: { $gt: Date.now() },
+    });
+
+    if (!user)
+      return res
+        .status(StatusCodes.CONFLICT)
+        .json({ message: "Invalid or expired verification code." });
+
+    // Verify the user
+    user.isVerified = true;
+    user.verificationToken = undefined;
+
+    await user.save();
+
+    return res.status(StatusCodes.OK).json({
+      message: "User verified successfully!",
+    });
+  } catch (error) {
+    console.log("Error in verifyAccount: ", error);
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json({ message: "Something went wrong. Please try again later." });
