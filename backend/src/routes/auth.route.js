0ce2ff60007d1426282737bdfba8002646611790
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-05-27 10:00:03 +0545

    Add routes for verify account, forgot and reset password

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,12 @@
 import express from "express";
 import {
   adminSignup,
+  forgotPassword,
   logout,
+  resetPassword,
   signin,
   signup,
+  verifyAccount,
 } from "../controllers/auth.controller.js";
 import authorizeRoles from "../middlewares/authorize.middleware.js";
 import authenticate from "../middlewares/authenticate.middleware.js";
@@ -22,5 +25,11 @@ authRouter.post(
 authRouter.post("/auth/signin", signin);
 
 authRouter.post("/auth/logout", logout);
+
+authRouter.post("/auth/verify-account/:token", verifyAccount);
+
+authRouter.post("/auth/forgot-password", forgotPassword);
+
+authRouter.post("/auth/reset-password/:token", resetPassword);
 
 export default authRouter;
