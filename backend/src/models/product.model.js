536af6c9789b8ed3eb055faadac1a6b4e52e64ad
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-05-27 14:18:35 +0545

    Create product model

---
+++ backend/src/models/product.model.js
@@ -0,0 +1,83 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    images: {
+      type: [String],
+      default: [],
+    },
+
+    discount: {
+      type: Number,
+      default: 0,
+    },
+
+    sizes: {
+      type: [String],
+      default: [],
+    },
+
+    colors: {
+      type: [String],
+      default: [],
+    },
+
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    ratings: {
+      type: Number,
+      default: 0,
+    },
+
+    reviews: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "review",
+    },
+
+    numReviews: {
+      type: Number,
+      default: 0,
+    },
+
+    isFeatured: {
+      type: Boolean,
+      default: false,
+    },
+
+    category: {
+      type: String,
+      required: true,
+      enum: ["men", "women", "kids"],
+    },
+
+    subCategory: {
+      type: String,
+      required: true,
+      enum: ["topwear", "bottomwear"],
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
