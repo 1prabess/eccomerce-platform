eb8f95153efd80786d9e7ee2fac737d39279fd57
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-05-28 18:32:49 +0545

    Add update product route

--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -3,8 +3,15 @@ import { createProduct } from "../controllers/product/createProduct.controller.j
 import authenticate from "../middlewares/authenticate.middleware.js";
 import authorizeRoles from "../middlewares/authorize.middleware.js";
 import { getProduct } from "../controllers/product/getProduct.controller.js";
+import { updateProduct } from "../controllers/product/updateProduct.controller.js";
+import { deleteProduct } from "../controllers/product/deleteProduct.controller.js";
+import { getProducts } from "../controllers/product/getProducts.controller.js";
 
 const productRouter = express.Router();
+
+productRouter.get("/products/:slug", getProduct);
+
+productRouter.get("/products", getProducts);
 
 productRouter.post(
   "/products",
@@ -13,6 +20,11 @@ productRouter.post(
   createProduct
 );
 
-productRouter.get("/products/:slug", getProduct);
+productRouter.patch(
+  "/products/:id",
+  authenticate,
+  authorizeRoles("admin"),
+  updateProduct
+);
 
 export default productRouter;
