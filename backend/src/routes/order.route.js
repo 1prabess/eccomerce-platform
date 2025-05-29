e0f7f32f555d3b0fd11700265d77459a0eb63afe
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-05-29 13:37:02 +0545

    Add route to get all orders by admin

--- backend/src/routes/order.route.js
+++ backend/src/routes/order.route.js
@@ -2,9 +2,12 @@ import express from "express";
 import { createOrder } from "../controllers/order/createOrder.controller.js";
 import authenticate from "../middlewares/authenticate.middleware.js";
 import { validateOrder } from "../middlewares/validateOrder.middleware.js";
+import { getOrders } from "../controllers/order/getOrders.controller.js";
+import authorizeRoles from "../middlewares/authorize.middleware.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/orders", authenticate, validateOrder, createOrder);
+orderRouter.get("/orders", authenticate, authorizeRoles("admin"), getOrders);
 
 export default orderRouter;
