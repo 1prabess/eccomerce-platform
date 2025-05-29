f8e1fda455f9ad6bcc80e4fd791bd7c9e3597947
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-05-29 12:03:44 +0545

    Create order model

---
+++ backend/src/models/order.model.js
@@ -0,0 +1,94 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+
+    products: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "product",
+          required: true,
+        },
+
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+
+        priceAtPurchase: {
+          type: Number,
+          required: true,
+          min: 0,
+        },
+      },
+    ],
+
+    totalPrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    shippingAddress: {
+      street: {
+        type: String,
+        required: true,
+      },
+
+      city: {
+        type: String,
+        required: true,
+      },
+
+      state: {
+        type: String,
+        required: true,
+      },
+
+      postalCode: {
+        type: String,
+        required: true,
+      },
+
+      country: {
+        type: String,
+        required: true,
+      },
+    },
+
+    paymentMethod: {
+      type: String,
+      enum: ["digital", "cash"],
+      required: true,
+      default: "digital",
+    },
+
+    paymentStatus: {
+      type: String,
+      enum: ["pending", "paid", "failed"],
+      default: "pending",
+    },
+
+    orderStatus: {
+      type: String,
+      enum: ["pending", "processing", "shipped", "delivered", "canceled"],
+      default: "pending",
+    },
+
+    notes: {
+      type: String,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
