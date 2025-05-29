38ca24b25908a9646d2d91587621e22d7dc7d2a4
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-05-29 14:38:36 +0545

    Add get own orders and specific orders route

--- backend/src/routes/order.route.js
+++ backend/src/routes/order.route.js
@@ -4,10 +4,25 @@ import authenticate from "../middlewares/authenticate.middleware.js";
 import { validateOrder } from "../middlewares/validateOrder.middleware.js";
 import { getOrders } from "../controllers/order/getOrders.controller.js";
 import authorizeRoles from "../middlewares/authorize.middleware.js";
+import { getMyOrders } from "../controllers/order/getMyOrders.controller.js";
+import { getOrder } from "../controllers/order/getOrder.controller.js";
+import { updateOrderStatus } from "../controllers/order/updateOrderStatus.controller.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/orders", authenticate, validateOrder, createOrder);
+
 orderRouter.get("/orders", authenticate, authorizeRoles("admin"), getOrders);
+
+orderRouter.get("/orders/my", authenticate, getMyOrders);
+
+orderRouter.get("/orders/:id", authenticate, getOrder);
+
+orderRouter.patch(
+  "/orders/:id/status",
+  authenticate,
+  authorizeRoles("admin"),
+  updateOrderStatus
+);
 
 export default orderRouter;
