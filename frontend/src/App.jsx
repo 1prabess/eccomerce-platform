c584f30cfe267c58b9a750cb15030c529568acd1
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-05-30 13:15:53 +0545

    Configure shadcn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,11 @@
-const App = () => {
-  return <div className="text-rose-500">App</div>;
-};
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
+}
 
 export default App;
