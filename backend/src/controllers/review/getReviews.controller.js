1bc9ac596fe03c603fb97139cb225bcbb4857c94
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-05-30 12:53:05 +0545

    Add get reviews with filtering

---
+++ backend/src/controllers/review/getReviews.controller.js
@@ -0,0 +1,36 @@
+import { StatusCodes } from "http-status-codes";
+import Review from "../../models/review.model.js";
+
+// ___________Get Reviews_________________
+export const getReviews = async (req, res) => {
+  try {
+    const { productId } = req.params;
+
+    const { rating } = req.query;
+
+    let query = {
+      productId,
+    };
+
+    if (rating) query.rating = rating;
+
+    // Check if productId is given or not
+    if (!productId)
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: "Product ID is required.",
+      });
+
+    // Fetch reviews
+    const reviews = await Review.find(query);
+
+    return res.status(StatusCodes.OK).json({
+      message: "Reviews fetched successfully!",
+      reviews,
+    });
+  } catch (error) {
+    console.log("Error in getReviews: ", error);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Something went wrong. Please try again later." });
+  }
+};
