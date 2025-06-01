8b93c8297215f5f802b2dcf25dd56a806b227724
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-01 13:59:40 +0545

    Create homepage

---
+++ frontend/src/features/home/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import Hero from "../components/Hero";
+import LatestCollections from "../components/LatestCollections";
+
+function HomePage() {
+  return (
+    <div className="box">
+      <Hero />
+      <LatestCollections />
+    </div>
+  );
+}
+
+export default HomePage;
