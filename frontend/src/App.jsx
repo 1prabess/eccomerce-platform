207ec39e93816c6ae2475ed71391a595848a7e76
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-01 14:00:21 +0545

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div>
+      <Navbar />
+      <Outlet />
     </div>
   );
 }
