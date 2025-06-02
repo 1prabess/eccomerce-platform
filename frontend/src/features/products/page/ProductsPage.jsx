bd80e2a5cca3ff9ad9f44ce82ead1c9b47a97aaa
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-02 14:38:21 +0545

    Create ProductsPage.jsx

---
+++ frontend/src/features/products/page/ProductsPage.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import Products from "../components/Products";
+
+const ProductsPage = () => {
+  const [filters, setFilters] = useState({
+    category: "",
+    subCategory: "",
+  });
+
+  return (
+    <div className="box flex flex-col gap-6 p-6 lg:flex-col">
+      <div className="w-full">
+        <Products filters={filters} setFilters={setFilters} />
+      </div>
+    </div>
+  );
+};
+
+export default ProductsPage;
