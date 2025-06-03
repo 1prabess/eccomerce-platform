f45d1e7e03d5a9c22d8936cc1dd3f1c8d119981f
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-03 11:35:42 +0545

    Add getProduct and getReviews

--- frontend/src/api/products.js
+++ frontend/src/api/products.js
@@ -16,3 +16,27 @@ export const getProducts = async ({ limit, category, subCategory, page }) => {
     );
   }
 };
+
+export const getProduct = async (productSlug) => {
+  try {
+    const response = await axiosInstance.get(`/products/${productSlug}`);
+    return response.data.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message || "Failed to fetch product");
+  }
+};
+
+export const getReviews = async (productId, rating) => {
+  try {
+    let params = {};
+
+    if (rating) params.rating = rating;
+
+    const response = await axiosInstance.get(`/products/${productId}/reviews`, {
+      params,
+    });
+    return response.data.data.reviews;
+  } catch (error) {
+    throw new Error(error.response?.data?.message || "Failed to fetch reviews");
+  }
+};
