b4fa0b9547d2248e194179a4b8de87abc7720894
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-03 11:35:55 +0545

    Add auto scroll component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <ScrollToTop />
       <Outlet />
     </div>
   );
