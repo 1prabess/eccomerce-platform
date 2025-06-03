7a009431cc8412c2a8a6f6e6aeec5a1c9cfbc748
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-03 11:38:48 +0545

    Add hover animation and fix some stylings

--- frontend/src/features/home/components/LatestProducts.jsx
+++ frontend/src/features/home/components/LatestProducts.jsx
@@ -1,11 +1,29 @@
+import Spinner from "@/components/Spinner";
 import { useProducts } from "@/hooks/products/useProducts";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const LatestProducts = () => {
-  const { data } = useProducts({
+  const { data, isPending, error } = useProducts({
     limit: 6,
     page: 1,
   });
+
+  const navigate = useNavigate();
+
+  const handleSelectProduct = (productSlug) => {
+    navigate(`/products/${productSlug}`);
+  };
+
+  if (isPending) return <Spinner />;
+
+  if (error) {
+    return (
+      <div className="mt-10 text-center text-red-500">
+        Failed to load products. Please try again later.
+      </div>
+    );
+  }
 
   return (
     <div className="mt-10">
@@ -21,19 +39,27 @@ const LatestProducts = () => {
 
       <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3">
         {data?.products.map((product, idx) => (
-          <div key={idx} className="border">
-            <img
-              src={product.images[0]}
-              alt={product.title}
-              className="w-full bg-white object-cover"
-            />
+          <div
+            key={idx}
+            className="cursor-pointer overflow-hidden border"
+            onClick={() => handleSelectProduct(product.slug)}
+          >
+            <div className="group relative overflow-hidden">
+              <img
+                src={product.images[0]}
+                alt={product.title}
+                className="w-full bg-white object-cover transition-transform duration-500 group-hover:scale-105"
+              />
+              <div className="absolute inset-0 bg-black/10 opacity-0 transition-opacity duration-300 group-hover:opacity-20" />
+            </div>
+
             <div className="flex justify-between p-4">
-              <h3 className="mt-2 text-lg">{product.name}</h3>
+              <h3 className="mt-2 text-xl">{product.name}</h3>
               <h3 className="mt-2 text-sm text-gray-500 uppercase">
                 {product.category}
               </h3>
             </div>
-            <p className="px-4 pb-4 text-3xl font-medium text-gray-800">
+            <p className="px-4 pb-6 text-3xl font-medium text-gray-500">
               ${product.price}
             </p>
           </div>
