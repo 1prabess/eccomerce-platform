2bb1411079df19ce7d737c4f4a6ed6fdc8b946a8
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-05 12:10:27 +0545

    Create auth.js

---
+++ frontend/src/api/auth.js
@@ -0,0 +1,54 @@
+import { axiosInstance } from "@/lib/axios";
+
+export const signin = async (creds) => {
+  try {
+    const response = await axiosInstance.post("/auth/signin", creds);
+    return response.data.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const signup = async (creds) => {
+  try {
+    const response = await axiosInstance.post("/auth/signup", creds);
+    return response.data.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const verifyAccount = async (code) => {
+  try {
+    const response = await axiosInstance.post(`/auth/verify-account/${code}`);
+    return response.data.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const forgotPassword = async (email) => {
+  try {
+    const response = await axiosInstance.post("/auth/forgot-password", {
+      email,
+    });
+    return response.data.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const resetPassword = async ({ resetPasswordToken, password }) => {
+  console.log(resetPasswordToken);
+  try {
+    const response = await axiosInstance.post(
+      `/auth/reset-password/${resetPasswordToken}`,
+      {
+        newPassword: password,
+      },
+    );
+    return response.data.data;
+  } catch (error) {
+    throw error;
+  }
+};
