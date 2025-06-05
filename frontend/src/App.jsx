8cfcfa6ba0f7be3449c109940d003289e83be0ba
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-05 12:10:10 +0545

    Add react hot toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ScrollToTop from "./components/ScrollToTop";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
@@ -8,6 +9,17 @@ function App() {
       <Navbar />
       <ScrollToTop />
       <Outlet />
+      <Toaster
+        reverseOrder={false}
+        toastOptions={{
+          success: {
+            iconTheme: {
+              primary: "black",
+              secondary: "white",
+            },
+          },
+        }}
+      />
     </div>
   );
 }
