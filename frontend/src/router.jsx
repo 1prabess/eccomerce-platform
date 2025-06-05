e6ac8b95b7e41df55940d54ec6fc26f668385f44
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-05 12:08:51 +0545

    Add signup, forgotpassword and resetpassword page route

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import HomePage from "./features/home/pages/HomePage";
-import Login from "./features/auth/pages/Login";
-import Signup from "./features/auth/pages/Signup";
 import ProductsPage from "./features/products/page/ProductsPage";
 import ProductDetailsPage from "./features/products/page/ProductDetailPage";
+import SigninPage from "./features/auth/pages/SigninPage";
+import SignupPage from "./features/auth/pages/SignupPage";
+import VerificationPage from "./features/auth/pages/VerificationPage";
+import ForgotPasswordPage from "./features/auth/pages/ForgotPasswordPage";
+import ResetPasswordPage from "./features/auth/pages/ResetPasswordPage";
 
 export const router = createBrowserRouter([
   {
@@ -17,11 +20,20 @@ export const router = createBrowserRouter([
       },
 
       {
-        path: "/login",
-        Component: Login,
+        path: "/signin",
+        Component: SigninPage,
       },
 
-      { path: "/signup", Component: Signup },
+      { path: "/signup", Component: SignupPage },
+
+      { path: "/forgot-password", Component: ForgotPasswordPage },
+
+      {
+        path: "/reset-password/:resetPasswordToken",
+        Component: ResetPasswordPage,
+      },
+
+      { path: "/verify-account", Component: VerificationPage },
 
       {
         path: "/products",
