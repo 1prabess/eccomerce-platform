fab791dd447f2370e40722b5ecbd04021d655de8
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-07 18:00:52 +0545

    Add update profile route

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,21 @@
 import express from "express";
 import { getProfile } from "../controllers/user/getProfile.controller.js";
 import authenticate from "../middlewares/authenticate.middleware.js";
+import multer from "multer";
+import { updateProfileController } from "../controllers/user/updateProfile.controller.js";
+
+// Store files temporarily in a "temp/" directory
+const upload = multer({ dest: "temp/" });
 
 const userRouter = express.Router();
 
 userRouter.get("/user/profile", authenticate, getProfile);
+
+userRouter.patch(
+  "/user/profile",
+  authenticate,
+  upload.single("image"),
+  updateProfileController
+);
 
 export default userRouter;
