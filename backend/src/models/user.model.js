a87cbe9b10c9a6311caeee2bd81c83106c3811d3
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-07 18:01:01 +0545

    Add profilePic and address

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -26,6 +26,11 @@ const userSchema = new mongoose.Schema(
       default: "user",
     },
 
+    profilePic: {
+      type: String,
+      default: "",
+    },
+
     isVerified: {
       type: String,
       default: false,
@@ -45,6 +50,14 @@ const userSchema = new mongoose.Schema(
 
     resetPasswordTokenExpiresAt: {
       type: Date,
+    },
+
+    address: {
+      street: { type: String, default: "" },
+      city: { type: String, default: "" },
+      state: { type: String, default: "" },
+      zip: { type: String, default: "" },
+      country: { type: String, default: "" },
     },
   },
   { timestamps: true, versionKey: false }
