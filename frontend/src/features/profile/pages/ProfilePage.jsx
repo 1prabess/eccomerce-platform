4dcc2c6d3edddadcfedbffb673832bc4849b85ce
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-07 18:00:23 +0545

    Create ProfilePage.jsx

---
+++ frontend/src/features/profile/pages/ProfilePage.jsx
@@ -0,0 +1,14 @@
+import AccountInfo from "../components/AccountInfo";
+
+const ProfilePage = () => {
+  return (
+    <div className="box mt-4 grid grid-cols-2">
+      <div>
+        <AccountInfo />
+      </div>
+      <div>he</div>
+    </div>
+  );
+};
+
+export default ProfilePage;
