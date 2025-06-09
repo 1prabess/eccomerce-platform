a2930ab5b9905b35256383a73cbd4386305c19fe
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-09 08:35:09 +0545

    Create getCartItems.controller.js

---
+++ backend/src/controllers/cart/getCartItems.controller.js
@@ -0,0 +1,19 @@
+import { StatusCodes } from "http-status-codes";
+import Cart from "../../models/cart.model.js";
+
+export const getCartItems = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const cartItems = await Cart.find({ userId }).populate("productId");
+
+    return res
+      .status(StatusCodes.OK)
+      .json({ cartItems, message: "Cart fetched successfully!" });
+  } catch (error) {
+    console.error("Error in getCartItems: ", error);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Something went wrong. Please try again later." });
+  }
+};
