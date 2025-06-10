0c4d6ab5a6daab748af9da7e67b8c881f0dcd295
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-10 13:20:23 +0545

    Update CartTotals.jsx

--- frontend/src/features/cart/components/CartTotals.jsx
+++ frontend/src/features/cart/components/CartTotals.jsx
@@ -1,31 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
 function CartTotals({ items }) {
-  const subtotal = items.reduce(
+  const navigate = useNavigate();
+
+  const total = items.reduce(
     (sum, item) => sum + item.price * item.quantity,
     0,
   );
-  const shipping = 10;
-  const total = subtotal + shipping;
 
   return (
-    <div className="mt-8 ml-auto max-w-md">
-      <h3 className="mb-4 border-b pb-2 text-xl font-semibold">Cart Totals</h3>
-
-      <div className="mb-2 flex justify-between">
-        <span>Subtotal</span>
-        <span>${subtotal.toFixed(2)}</span>
+    <div className="mt-4 w-full items-center md:mt-4">
+      <div className="flex justify-end">
+        <button
+          onClick={() => navigate("/order")}
+          className="w-fit bg-black px-4 py-3 text-white hover:bg-gray-800"
+        >
+          Checkout: ${total.toFixed(2)}
+        </button>
       </div>
-      <div className="mb-2 flex justify-between">
-        <span>Shipping Fee</span>
-        <span>${shipping.toFixed(2)}</span>
-      </div>
-      <div className="flex justify-between font-bold text-black">
-        <span>Total</span>
-        <span>${total.toFixed(2)}</span>
-      </div>
-
-      <button className="mt-6 w-full rounded bg-black py-3 text-white hover:bg-gray-800">
-        Proceed to Checkout
-      </button>
     </div>
   );
 }
