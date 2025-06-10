1cb5a80bcd5bd5f115032bf5b6e5f851c7147bd5
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-10 13:19:45 +0545

    Create useIncreaseItemQuantity.js

---
+++ frontend/src/hooks/cart/useIncreaseItemQuantity.js
@@ -0,0 +1,14 @@
+import { updateItemQuantity } from "@/api/cart";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+export const useIncreaseQuantity = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (updateData) => updateItemQuantity(updateData),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["cart"]);
+    },
+    onError: (error) => toast.error(error),
+  });
+};
