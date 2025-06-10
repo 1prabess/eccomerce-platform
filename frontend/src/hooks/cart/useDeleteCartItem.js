d596e6fee937c330025a09cd0961f3a2c1cb37c0
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-10 13:19:48 +0545

    Create useDeleteCartItem.js

---
+++ frontend/src/hooks/cart/useDeleteCartItem.js
@@ -0,0 +1,12 @@
+import { deleteCartItem } from "@/api/cart";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+export const useDeleteCartItem = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (cartItemId) => deleteCartItem(cartItemId),
+    onSuccess: () => queryClient.invalidateQueries(["cart"]),
+    onError: (error) => toast.error(error),
+  });
+};
