5fd14f30f53ad0a3df52773bee963c03b0b4f34d
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-15 15:02:39 +0545

    Add fixed footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,31 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ScrollToTop from "./components/ScrollToTop";
 import { Toaster } from "react-hot-toast";
+import Footer from "./features/home/components/Footer";
+import { useCheckAuth } from "./hooks/auth/useCheckAuth";
+import { useDispatch } from "react-redux";
+import { setUser } from "./store/authSlice";
 
 function App() {
+  const { data } = useCheckAuth();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (data?.user != null) {
+      dispatch(setUser(data));
+    }
+  }, [data, dispatch]);
+
   return (
     <div>
       <Navbar />
       <ScrollToTop />
       <Outlet />
+      <div className="box">
+        <Footer />
+      </div>
       <Toaster
         reverseOrder={false}
         toastOptions={{
