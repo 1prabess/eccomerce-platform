164dcd30124233bee87d384b53cbb6acfef6e8e6
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-15 15:00:37 +0545

    Add new routes

--- backend/src/routes/cart.route.js
+++ backend/src/routes/cart.route.js
@@ -2,11 +2,20 @@ import express from "express";
 import authenticate from "../middlewares/authenticate.middleware.js";
 import { getCartItems } from "../controllers/cart/getCartItems.controller.js";
 import { addItemToCart } from "../controllers/cart/addItemToCart.controller.js";
+import { updateCartItemQuantity } from "../controllers/cart/updateCartItemQuantity.controller.js";
+import { deleteCartItem } from "../controllers/cart/deleteCartItem.controller.js";
+import { resetCart } from "../controllers/cart/resetCart.controller.js";
 
 const cartRouter = express.Router();
 
 cartRouter.get("/cart", authenticate, getCartItems);
 
-cartRouter.post("/cart/add", authenticate, addItemToCart);
+cartRouter.post("/cart", authenticate, addItemToCart);
+
+cartRouter.patch("/cart/update", authenticate, updateCartItemQuantity);
+
+cartRouter.delete("/cart/deleteItem", authenticate, deleteCartItem);
+
+cartRouter.delete("/cart", authenticate, resetCart);
 
 export default cartRouter;
