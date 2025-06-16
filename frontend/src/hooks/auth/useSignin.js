ae8517c0d76754543c70b3a21ba2430203ab197d
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-16 14:23:57 +0545

    Update useSignin.js

--- frontend/src/hooks/auth/useSignin.js
+++ frontend/src/hooks/auth/useSignin.js
@@ -1,11 +1,16 @@
 import { signin } from "@/api/auth";
+import { setUser } from "@/store/authSlice";
 import { useMutation } from "@tanstack/react-query";
 import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
 
 export const useSignin = () => {
+  const dispatch = useDispatch();
+
   return useMutation({
     mutationFn: (creds) => signin(creds),
-    onSuccess: () => {
+    onSuccess: (user) => {
+      dispatch(setUser(user.user));
       toast.success("Logged in successfully!");
     },
     onError: (error) => {
