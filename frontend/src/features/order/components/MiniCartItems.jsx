9645d74aef8bcc3dd414dcfb3a39a17a75827053
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-16 14:25:44 +0545

    Create small cart displaying component

---
+++ frontend/src/features/order/components/MiniCartItems.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+function MiniCartItems({ items = [] }) {
+  return (
+    <div className="mb-6 space-y-1 border p-4">
+      <h3 className="text-lg font-semibold text-gray-800">Items in Cart</h3>
+
+      {items.map((item) => (
+        <div
+          key={item._id}
+          className="flex items-center gap-4 border-b pb-3 last:border-b-0"
+        >
+          <img
+            src={item.productId.images?.[0]}
+            alt={item.productId.name}
+            className="h-fit w-16 object-cover"
+          />
+
+          <div className="flex-1">
+            <h4 className="text-sm font-medium text-gray-800">
+              {item.productId.name}
+            </h4>
+            <p className="text-sm text-gray-500">
+              Size: {item.size} | Qty: {item.quantity}
+            </p>
+          </div>
+
+          <p className="text-sm font-semibold text-gray-700">
+            ${item.priceAtAddTime * item.quantity}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default MiniCartItems;
