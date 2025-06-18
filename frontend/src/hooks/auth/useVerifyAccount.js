0fe31b5fc6956073b6fadf940dcf54644d55fa6f
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-18 13:46:28 +0545

    Navigate to homepage

--- frontend/src/hooks/auth/useVerifyAccount.js
+++ frontend/src/hooks/auth/useVerifyAccount.js
@@ -1,11 +1,15 @@
 import { verifyAccount } from "@/api/auth";
 import { useMutation } from "@tanstack/react-query";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export const useVerifyAccount = () => {
+  const navigate = useNavigate();
+
   return useMutation({
     mutationFn: (code) => verifyAccount(code),
     onSuccess: () => {
+      navigate("/");
       toast.success("Account verified successfully!");
     },
     onError: (error) => {
