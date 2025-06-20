c9b968ff9a0c194acb3e9676f1e5455ad0b577fd
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-20 10:36:26 +0545

    Update products.js

--- frontend/src/api/products.js
+++ frontend/src/api/products.js
@@ -9,11 +9,9 @@ export const getProducts = async ({ limit, category, subCategory, page }) => {
     if (page) params.page = page;
 
     const response = await axiosInstance.get("/products", { params });
-    return response.data.data;
+    return response.data;
   } catch (error) {
-    throw new Error(
-      error.response?.data?.message || "Failed to fetch products",
-    );
+    throw error;
   }
 };
 
@@ -22,7 +20,7 @@ export const getProduct = async (productSlug) => {
     const response = await axiosInstance.get(`/products/${productSlug}`);
     return response.data.data;
   } catch (error) {
-    throw new Error(error.response?.data?.message || "Failed to fetch product");
+    throw error;
   }
 };
 
@@ -31,12 +29,58 @@ export const getReviews = async (productId, rating) => {
     let params = {};
 
     if (rating) params.rating = rating;
-
     const response = await axiosInstance.get(`/products/${productId}/reviews`, {
       params,
     });
     return response.data.data.reviews;
   } catch (error) {
-    throw new Error(error.response?.data?.message || "Failed to fetch reviews");
+    throw error;
+  }
+};
+
+export const createProduct = async (productData) => {
+  try {
+    const response = await axiosInstance.post("/products", productData);
+    return response.data.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteProduct = async (productID) => {
+  try {
+    const response = await axiosInstance.delete(`/products/${productID}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateProduct = async (data) => {
+  try {
+    const { productID, updateData } = data;
+
+    const response = await axiosInstance.patch(
+      `/products/${productID}`,
+      updateData,
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const createReview = async (data) => {
+  try {
+    const { productID, review } = data;
+
+    const response = await axiosInstance.post(
+      `/products/${productID}/reviews`,
+      review,
+    );
+
+    return response.data;
+  } catch (error) {
+    throw error;
   }
 };
