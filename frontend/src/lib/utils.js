8eac11e147428c1f103f0b1731b967ec48af3244
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-20 10:37:01 +0545

    Update utils.js

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export function generateUniqueId() {
+  return `id-${Date.now()}-${Math.random().toString(36).substr(2, 9)}`;
+}
+
+export function base64Decode(base64) {
+  if (!base64) {
+    console.error("Base64 string is null or undefined");
+    return null;
+  }
+  try {
+    const standardBase64 = base64.replace(/-/g, "+").replace(/_/g, "/");
+    const decoded = atob(standardBase64);
+    return JSON.parse(decoded);
+  } catch (error) {
+    console.error("Error decoding base64 string:", error);
+    return null;
+  }
+}
