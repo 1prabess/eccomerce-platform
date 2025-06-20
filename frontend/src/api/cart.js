b5bba1ccc8467721c9f7b235c51ced7913c8285a
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-20 10:36:01 +0545

    Update cart.js

--- frontend/src/api/cart.js
+++ frontend/src/api/cart.js
@@ -1,13 +1,48 @@
 import { axiosInstance } from "@/lib/axios";
 
+export const addItemToCart = async (data) => {
+  try {
+    const response = await axiosInstance.post("/cart", data);
+    return response.data.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
 export const getCartItems = async () => {
   try {
     const response = await axiosInstance.get("/cart");
-
     return response.data.data;
   } catch (error) {
-    throw new Error(
-      error.response?.data?.message || "Failed to fetch cart item",
-    );
+    throw error;
+  }
+};
+
+export const updateItemQuantity = async (updateData) => {
+  try {
+    const response = await axiosInstance.patch("/cart/update", updateData);
+    return response.data.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteCartItem = async (cartItemId) => {
+  try {
+    const response = await axiosInstance.delete("/cart/deleteItem", {
+      data: { cartItemId },
+    });
+    return response.data.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const resetCart = async () => {
+  try {
+    const response = await axiosInstance.delete("/cart");
+    return response.data.data;
+  } catch (error) {
+    throw error;
   }
 };
