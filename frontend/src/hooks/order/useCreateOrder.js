c4c9b65f59ace0c19d6151531b0af030250ee392
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-06-21 16:07:49 +0545

    Add payment initiation after order creation
    After successfully creating an order, initiate payment via Esewa or Khalti based on the selected payment method. This ensures the payment process starts automatically for supported gateways.

--- frontend/src/hooks/order/useCreateOrder.js
+++ frontend/src/hooks/order/useCreateOrder.js
@@ -2,15 +2,42 @@ import { createOrder } from "@/api/order";
 import { useMutation } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { useResetCart } from "../cart/useResetCart";
+import { useInitiatePayment } from "../payment/useInitiatePayment";
 
 export const useCreateOrder = () => {
   const { mutate: resetCart } = useResetCart();
+  const { mutate: initiatePayment } = useInitiatePayment();
 
   return useMutation({
     mutationFn: createOrder,
-    onSuccess: () => {
+    onSuccess: (order) => {
       resetCart();
       toast.success("Order placed successfully!");
+      if (order.paymentMethod === "esewa") {
+        const paymentData = {
+          orderId: order._id,
+          customerName: order.fullName,
+          customerEmail: order.email,
+          customerPhone: order.phone,
+          amount: order.totalPrice,
+          paymentGateway: "esewa",
+        };
+
+        initiatePayment(paymentData);
+      }
+
+      if (order.paymentMethod === "khalti") {
+        const paymentData = {
+          orderId: order._id,
+          customerName: order.fullName,
+          customerEmail: order.email,
+          customerPhone: order.phone,
+          amount: order.totalPrice,
+          paymentGateway: "khalti",
+        };
+
+        initiatePayment(paymentData);
+      }
     },
     onError: (error) => {
       toast.error(error?.response?.data?.message || "Order failed. Try again.");
